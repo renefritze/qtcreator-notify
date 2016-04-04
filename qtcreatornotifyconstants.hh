0799e1fd681dc1832b2d67023287cd2fad01403a
Rene Milk <rene.milk@uni-muenster.de>
2016-04-04 11:55:53 +0200

    initial project from wizard

---
+++ qtcreatornotifyconstants.hh
@@ -0,0 +1,14 @@
+#ifndef QTCREATORNOTIFYCONSTANTS_HH
+#define QTCREATORNOTIFYCONSTANTS_HH
+
+namespace QtCreatorNotify {
+namespace Constants {
+
+const char ACTION_ID[] = "QtCreatorNotify.Action";
+const char MENU_ID[] = "QtCreatorNotify.Menu";
+
+} // namespace QtCreatorNotify
+} // namespace Constants
+
+#endif // QTCREATORNOTIFYCONSTANTS_HH
+
---
+++ qtcreatornotifyplugin.hh
@@ -0,0 +1,32 @@
+#ifndef QTCREATORNOTIFY_HH
+#define QTCREATORNOTIFY_HH
+
+#include "qtcreatornotify_global.hh"
+
+#include <extensionsystem/iplugin.h>
+
+namespace QtCreatorNotify {
+namespace Internal {
+
+class QtCreatorNotifyPlugin : public ExtensionSystem::IPlugin
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QtCreatorPlugin" FILE "QtCreatorNotify.json")
+
+public:
+  QtCreatorNotifyPlugin();
+  ~QtCreatorNotifyPlugin();
+
+  bool initialize(const QStringList &arguments, QString *errorString);
+  void extensionsInitialized();
+  ShutdownFlag aboutToShutdown();
+
+private slots:
+  void triggerAction();
+};
+
+} // namespace Internal
+} // namespace QtCreatorNotify
+
+#endif // QTCREATORNOTIFY_HH
+
